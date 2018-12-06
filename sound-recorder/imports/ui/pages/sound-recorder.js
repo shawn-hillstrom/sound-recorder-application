ec28c4e0c93ac2c4118c43fce049e3b0d0ad644d
Brad <bradley.hendrickson@wsu.edu>
2018-12-05 17:02:17 -0800

    Converted audio to use Wavesurfer.js

--- sound-recorder/imports/ui/pages/sound-recorder.js
+++ sound-recorder/imports/ui/pages/sound-recorder.js
@@ -4,28 +4,85 @@ import '/client/lib/semantic-ui/definitions/modules/progress.js';
 /* Variables */
 let audioChunks; // Variable to store recorded chunks of audio.
 let selectedChannel = 1; // Variable to store the last clicked channel.
+let wavesurfer1;
+let wavesurfer2;
+let wavesurfer3;
+let wavesurfer4;
+let wavesurfer5;
 
-function recordToTrack(track, tracklink, blob) {
-  track.src = URL.createObjectURL(blob);
-  track.controls = true;
-  track.autoplay = true;
-  tracklink.href = track1.src;
+
+function createWaves () {
+  wavesurfer1 = WaveSurfer.create({
+    container: '#wave1', //string myContainer
+    waveColor: 'violet',
+    progressColor: 'purple'
+  });
+
+  wavesurfer2 = WaveSurfer.create({
+    container: '#wave2', //string myContainer
+    waveColor: 'violet',
+    progressColor: 'purple'
+  });
+
+  wavesurfer3 = WaveSurfer.create({
+    container: '#wave3', //string myContainer
+    waveColor: 'violet',
+    progressColor: 'purple'
+  });
+
+  wavesurfer4 = WaveSurfer.create({
+    container: '#wave4', //string myContainer
+    waveColor: 'violet',
+    progressColor: 'purple'
+  });
+
+  wavesurfer5 = WaveSurfer.create({
+    container: '#wave5', //string myContainer
+    waveColor: 'violet',
+    progressColor: 'purple'
+  });
+}
+
+Template.Sound_Recorder_Page.onRendered(function () {
+  createWaves();
+});
+
+function recordToTrack(tracklink, blob, wavesurfer) {
+  let  file = URL.createObjectURL(blob);
+  tracklink.href = file;
   tracklink.download = 'mp3';
   tracklink.innerHTML = 'download';
+
+  wavesurfer.load(file);
+}
+
+function assignSoundToWave(wave, file) {
+
+  wavesurfer.load(file);
+
+  function play() {
+    wavesurfer.play();
+  }
+  function pause() {
+    wavesurfer.pause();
+  }
+  wavesurfer.play();
+  document.getElementById("playMe").onclick = play;
+  document.getElementById("pauseMe").onclick = pause;
 }
 
 /* Delete a Track in Specified Channel */
 function deleteTrack(selectedChannel) {
   if (selectedChannel === 1) {
-    track1.setAttribute('src', '');
+    wavesurfer1.empty();
   } else if (selectedChannel === 2) {
-    track2.setAttribute('src', '');
+    wavesurfer2.empty();
   } else if (selectedChannel === 3) {
-    track3.setAttribute('src', '');
+    wavesurfer3.empty();
   } else if (selectedChannel === 4) {
-    track4.setAttribute('src', '');
+    wavesurfer4.empty();
   } else {
-    track5.setAttribute('src', '');
+    wavesurfer5.empty();
   }
 }
 
@@ -58,11 +115,11 @@ Template.Sound_Recorder_Page.events({
   },
   /* Play Button Clicked */
   'click #play': function() {
-    track1.play();
-    track2.play();
-    track3.play();
-    track4.play();
-    track5.play();
+    wavesurfer1.play();
+    wavesurfer2.play();
+    wavesurfer3.play();
+    wavesurfer4.play();
+    wavesurfer5.play();
     track1.addEventListener('stop', function(e) {
     track1.play();
     });
@@ -73,11 +130,11 @@ Template.Sound_Recorder_Page.events({
 */
   /* Pause Button Clicked */
   'click #pause': function() {
-    track1.pause();
-    track2.pause();
-    track3.pause();
-    track4.pause();
-    track5.pause();
+    wavesurfer1.pause();
+    wavesurfer2.pause();
+    wavesurfer3.pause();
+    wavesurfer4.pause();
+    wavesurfer5.pause();
   },
   /* Record Button Clicked */
   'click #record': function() {
@@ -86,17 +143,58 @@ Template.Sound_Recorder_Page.events({
   },
   /* Stop Button Clicked */
   'click #stop': function() {
+    wavesurfer1.stop();
+    wavesurfer2.stop();
+    wavesurfer3.stop();
+    wavesurfer4.stop();
+    wavesurfer5.stop();
     rec.stop();
-    track1.stop();
-    track2.stop();
-    track3.stop();
-    track4.stop();
-    track5.stop();
   },
   /* Clear Button Clicked */
   'click #clear': function() {
     deleteTrack(selectedChannel);
   },
+
+  'click #play1': function() {
+    wavesurfer1.play();
+  },
+
+  'click #play2': function() {
+    wavesurfer2.play();
+  },
+
+  'click #play3': function() {
+    wavesurfer3.play();
+  },
+
+  'click #play4': function() {
+    wavesurfer4.play();
+  },
+
+  'click #play5': function() {
+    wavesurfer5.play();
+  },
+
+  'click #pause1': function() {
+    wavesurfer1.pause();
+  },
+
+  'click #pause2': function() {
+    wavesurfer2.pause();
+  },
+
+  'click #pause3': function() {
+    wavesurfer3.pause();
+  },
+
+  'click #pause4': function() {
+    wavesurfer4.pause();
+  },
+
+  'click #pause5': function() {
+    wavesurfer5.pause();
+  },
+
 });
 
 /* Get Access to the Mic and Record */
@@ -107,15 +205,15 @@ navigator.mediaDevices.getUserMedia({audio:true}).then(stream => {
     if (rec.state == "inactive") {
       let blob = new Blob(audioChunks,{type:'audio/x-mpeg-3'});
       if (selectedChannel === 1) {
-        recordToTrack(track1, track1link, blob);
+        recordToTrack(track1link, blob, wavesurfer1);
       } else if (selectedChannel === 2) {
-        recordToTrack(track2, track2link, blob);
+        recordToTrack(track2link, blob, wavesurfer2);
       } else if (selectedChannel === 3) {
-        recordToTrack(track3, track3link, blob);
+        recordToTrack(track3link, blob, wavesurfer3);
       } else if (selectedChannel === 4) {
-        recordToTrack(track4, track4link, blob);
+        recordToTrack(track4link, blob, wavesurfer4);
       } else {
-        recordToTrack(track5, track5link, blob);
+        recordToTrack(track5link, blob, wavesurfer5);
       }
     }
   };
