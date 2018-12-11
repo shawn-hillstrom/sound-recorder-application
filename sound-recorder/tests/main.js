52d82c7b6bd45c224ec2a75b6f095f09ce2f12c4
jameskeirnan <43251907+jameskeirnan@users.noreply.github.com>
2018-12-11 00:02:08 -0800

    updated unit tests

--- sound-recorder/tests/main.js
+++ sound-recorder/tests/main.js
@@ -1,31 +1,38 @@
 var assert = chai.assert;
-import wavesurfer1 from '../imports/ui/pages/sound-recorder.js';
 /* eslint-env mocha, chai */
 /* global chai, sound-recorder */
+
+
 /* Basic Tests */
 
+
+describe('createWaves', function () {
+    it('waveContainer1', function() {
+        assert(wavesurfer1).to.have.container('#wave1');
+    });
+    it('waveColor1', function() {
+        assert(wavesurfer1).to.have.waveColor('#19fc05');
+    });
+    it('waveProgress1', function() {
+        assert(wavesurfer1).to.have.progressColor('#0faa01');
+    });
+});
 
 describe("sound-recorder", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
-    chai.assert.strictEqual(name, "sound-recorder");
+    assert.strictEqual(name, "sound-recorder");
   });
 
   if (Meteor.isClient) {
     it("client is not server", function () {
-      chai.assert.strictEqual(Meteor.isServer, false);
+      assert.strictEqual(Meteor.isServer, false);
     });
   }
 
   if (Meteor.isServer) {
     it("server is not client", function () {
-      chai.assert.strictEqual(Meteor.isClient, false);
+      assert.strictEqual(Meteor.isClient, false);
     });
   }
-});
-
-describe('createWaves', function () {
-    it('waveColor1', function() {
-        chai.assert(wavesurfer1).to.have.waveColor('#19fc05');
-    });
 });
